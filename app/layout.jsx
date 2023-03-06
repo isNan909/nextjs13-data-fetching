1f3971f9a4c8c1fb1aa76481ce944bd04dc585b9
jereconjota <jeremiasfigueroa69@gmail.com>
2023-03-06 17:53:25 -0300

    Update Next.js, React, and React-DOM dependencies, add metadata to layout.jsx, and refactor navbar.jsx.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,16 @@
 import "../styles/globals.css";
 import Navbar from './shared/navbar';
 
+export const metadata = {
+	title: 'Data Fetching',
+	icons: {
+		icon: '/favicon.ico'
+	}
+}
+  
 export default function Layout({ children }) {
 	return (
 		<html>
-			<head></head>
 			<body className="wrapper">
 				<Navbar />
 				{children}
